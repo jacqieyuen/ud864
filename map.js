c07b186a4e2200752b626a4296d05670858eff50
Jacquelyn Yuen <jacquelyn.yuen@quantifeed.com>
2019-06-14 12:18:02 +0800

    styled the map

--- map.js
+++ map.js
@@ -4,22 +4,277 @@ var map;
 var markers = [];
 
 function initMap() {
+	// Create styles array for map
+	var styles= [
+		{
+			"featureType": "all",
+			"elementType": "labels",
+			"stylers": [
+				{
+					"visibility": "on"
+				}
+			]
+		},
+		{
+			"featureType": "all",
+			"elementType": "labels.text.fill",
+			"stylers": [
+				{
+					"saturation": 36
+				},
+				{
+					"color": "#333333"
+				},
+				{
+					"lightness": 40
+				}
+			]
+		},
+		{
+			"featureType": "all",
+			"elementType": "labels.text.stroke",
+			"stylers": [
+				{
+					"visibility": "on"
+				},
+				{
+					"color": "#ffffff"
+				},
+				{
+					"lightness": 16
+				}
+			]
+		},
+		{
+			"featureType": "all",
+			"elementType": "labels.icon",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "administrative",
+			"elementType": "geometry.fill",
+			"stylers": [
+				{
+					"color": "#fefefe"
+				},
+				{
+					"lightness": 20
+				}
+			]
+		},
+		{
+			"featureType": "administrative",
+			"elementType": "geometry.stroke",
+			"stylers": [
+				{
+					"color": "#fefefe"
+				},
+				{
+					"lightness": 17
+				},
+				{
+					"weight": 1.2
+				}
+			]
+		},
+		{
+			"featureType": "administrative",
+			"elementType": "labels",
+			"stylers": [
+				{
+					"visibility": "on"
+				}
+			]
+		},
+		{
+			"featureType": "landscape",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"color": "#f5f5f5"
+				},
+				{
+					"lightness": 20
+				}
+			]
+		},
+		{
+			"featureType": "landscape",
+			"elementType": "labels",
+			"stylers": [
+				{
+					"visibility": "on"
+				}
+			]
+		},
+		{
+			"featureType": "poi",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"color": "#f5f5f5"
+				},
+				{
+					"lightness": 21
+				}
+			]
+		},
+		{
+			"featureType": "poi",
+			"elementType": "labels",
+			"stylers": [
+				{
+					"visibility": "on"
+				}
+			]
+		},
+		{
+			"featureType": "poi.park",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"color": "#dedede"
+				},
+				{
+					"lightness": 21
+				}
+			]
+		},
+		{
+			"featureType": "road.highway",
+			"elementType": "geometry.fill",
+			"stylers": [
+				{
+					"color": "#ffffff"
+				},
+				{
+					"lightness": 17
+				}
+			]
+		},
+		{
+			"featureType": "road.highway",
+			"elementType": "geometry.stroke",
+			"stylers": [
+				{
+					"color": "#ffffff"
+				},
+				{
+					"lightness": 29
+				},
+				{
+					"weight": 0.2
+				}
+			]
+		},
+		{
+			"featureType": "road.arterial",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"color": "#ffffff"
+				},
+				{
+					"lightness": 18
+				}
+			]
+		},
+		{
+			"featureType": "road.local",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"color": "#ffffff"
+				},
+				{
+					"lightness": 16
+				}
+			]
+		},
+		{
+			"featureType": "road.local",
+			"elementType": "labels.text",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "transit",
+			"elementType": "all",
+			"stylers": [
+				{
+					"visibility": "simplified"
+				}
+			]
+		},
+		{
+			"featureType": "transit",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"color": "#f2f2f2"
+				},
+				{
+					"lightness": 19
+				}
+			]
+		},
+		{
+			"featureType": "transit.station.bus",
+			"elementType": "all",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "water",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"color": "#e9e9e9"
+				},
+				{
+					"lightness": 17
+				}
+			]
+		}
+	]
+
 	// Constructor creates a new map - only center and zoom are required.
 	map = new google.maps.Map(document.getElementById("map"), {
-		center: { lat: 22.278088, lng: 114.181998 },
-		zoom: 13
+		center: {
+			lat: 22.278088,
+			lng: 114.181998
+		},
+		zoom: 13,
+		styles: styles,
+		mapTypeControl: false
 	});
 
 	// These are the real estate listings that will be shown to the user.
 	// Normally we'd have these in a database instead.
-    var locations = [
-        { title: "Samsen", location: { lat: 22.274208, lng: 114.174143 } },
-        { title: "HK Google Office", location: { lat: 22.278088, lng: 114.181998 } }
-    ];
+	var locations = [
+		{
+			title: "Samsen",
+			location: { lat: 22.274208, lng: 114.174143 }
+		},
+		{
+			title: "HK Google Office",
+			location: { lat: 22.278088, lng: 114.181998 }
+		}
+	];
 
 	var bounds = new google.maps.LatLngBounds();
-    var largeInfowindow = new google.maps.InfoWindow();
-    var image = "./assets/blue_marker_40.png";
+	var largeInfowindow = new google.maps.InfoWindow();
+	var image = "./assets/blue_marker_40.png";
 	// The following group uses the location array to create an array of markers on initialize.
 	for (var i = 0; i < locations.length; i++) {
 		// Get the position from the location array.
@@ -31,8 +286,8 @@ function initMap() {
 			position: position,
 			title: title,
 			animation: google.maps.Animation.DROP,
-            id: i,
-            icon: image
+			id: i,
+			icon: image
 		});
 		// Push the marker to our array of markers.
 		markers.push(marker);
@@ -54,7 +309,9 @@ function populateInfoWindow(marker, infowindow) {
 	// Check to make sure the infowindow is not already opened on this marker.
 	if (infowindow.marker != marker) {
 		infowindow.marker = marker;
-		infowindow.setContent("<div>" + marker.title + "</div><div>"+marker.position+"</div>");
+		infowindow.setContent(
+			"<div>" + marker.title + "</div><div>" + marker.position + "</div>"
+		);
 		infowindow.open(map, marker);
 		// Make sure the marker property is cleared if the infowindow is closed.
 		infowindow.addListener("closeclick", function() {
